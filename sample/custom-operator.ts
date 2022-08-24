9da33adaa5e05a1243a4dc03a9eefc1c01e26995
ajie <m18210870360@163.com>
2022-08-24 15:28:43 +0800

    implement custom operator

--- sample/custom-operator.ts
+++ sample/custom-operator.ts
@@ -5,10 +5,11 @@ import { Subscriber } from "../src/subscriber"
 const isEven = () => {
     return (source: Observable<any>) => {
         return new Observable<any>(observer => {
-            return source.subscribe((x) => {
+            const subscription = source.subscribe((x) => {
                 observer.next(x % 2 === 0);
                 observer.complete();
             })
+            return () => subscription.unsubscribe();
         })
     }
 }
@@ -16,26 +17,27 @@ const isEven = () => {
 const odd = () => {
     const operator: Operator<any, any> = {
         call(subscriber: Subscriber<any>, source: any) {
+            const subscription = source.subscribe((x: any) => subscriber.next(x % 2 === 0));
             return () => {
-                source.subscribe((x: any) => subscriber.next(x))
-                // subscriber.next(source % 2 === 0);
-                // subscriber.complete();
+                subscription.unsubscribe();
             };
-        }
+        },
     }
     return operator;
 }
 
-// new Observable(observer => {
-//     observer.next(7);
-// })
-//     .pipe(isEven())
-//     .subscribe(x => {
-//         console.log(x);
-//     });
+new Observable(observer => {
+    observer.next(7);
+})
+    .pipe(isEven())
+    .subscribe(x => {
+        console.log(x);
+    });
 
 new Observable(observer => {
     observer.next(7);
-}).lift(odd).subscribe(x => {
-    console.log(x);
 })
+    .lift(odd())
+    .subscribe(x => {
+        console.log(x);
+    })
---
+++ sample/scheduler.ts
@@ -0,0 +1,32 @@
+import { asapScheduler, asyncScheduler, from } from 'rxjs';
+
+
+function syncSchedulerMain() {
+    console.log('before');
+    from([1, 2, 3]).subscribe((x: any) => {
+        console.log(x);
+    })
+    console.log('after');
+}
+
+function asyncSchedulerMain() {
+    console.log('asyncScheduler: before');
+    from([1, 2, 3], asyncScheduler).subscribe((x: any) => {
+        console.log(x);
+    })
+    Promise.resolve('asyncScheduler: promise').then(console.log);
+    console.log('asyncScheduler: after');
+}
+
+function asapSchedulerMain() {
+    console.log('asapScheduler: before');
+    from([1, 2, 3], asapScheduler).subscribe((x: any) => {
+        console.log(x);
+    })
+    Promise.resolve('asapScheduler: promise').then(console.log);
+    console.log('asapScheduler: after');
+}
+
+// syncSchedulerMain();
+asyncSchedulerMain();
+asapSchedulerMain();
